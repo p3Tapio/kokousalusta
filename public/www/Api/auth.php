01a4d3efc1c58ce2249503a9cf8aa764639e88ea
Thwap1 <kuisma.sami@gmail.com>
2020-07-14 16:37:43 +0300

    php session user_id authiin

--- public/www/Api/auth.php
+++ public/www/Api/auth.php
@@ -1,4 +1,7 @@
-<?php
+<?php session_start();
+header("Access-Control-Allow-Origin:".$_SERVER['HTTP_ORIGIN']); 
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Headers: X-Accept-Charset,X-Accept,Content-Type,Authorization,Accept,Origin,Authorization, Origin, X-Requested-With, Content-Type, Accept, Accept-Language");
 
 $_POST = json_decode(file_get_contents('php://input'), true); 
 
@@ -170,7 +173,10 @@ function appLogin() {
                     $tulos->free();      
                     $token = tokenGen(); 
                     $response = array("message"=> "Kirjautuminen onnistui", "firstname" => "$firstname", "lastname" => "$lastname", "token"=>"$token");
-
+                    // TODO tässä php sessioniin user variable (muista nollata kun kirjautuu ulos)
+                    // TODO? logout
+                    // jos joskus logout niin.. unset($_SESSION['user_id']);
+                    $_SESSION['user_id'] = 2;
                 } else {
                     $response = array("message"=> "Väärä käyttäjätunnus tai salasana"); 
                     http_response_code(401); 
@@ -214,7 +220,7 @@ function checkUserEmail($email) {
     }
 }
 function connect() {
-    include("dbdetails.php");
+    include("../dbdetails.php");
     $yhteys = new mysqli($host, $user, $password, $db) or die("Connection fail ".mysqli_connect_error());
     $yhteys->set_charset("utf8");
     return $yhteys;
